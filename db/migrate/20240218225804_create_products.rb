2fdbc9ca2fbd37ab128fa6e464bf7b96f4c1fc55
maeci <cimafrancamae@gmail.com>
2024-02-19 07:00:07 +0800

    init commit

---
+++ db/migrate/20240218225804_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :desc
+      t.decimal :price
+      t.integer :qty
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
