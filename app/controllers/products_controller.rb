4c20733791018531c716e88c198e639d5f0c9a2f
maeci <cimafrancamae@gmail.com>
2024-02-19 09:22:01 +0800

    Add controller, views and tailwind css

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,50 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path, notice: 'Product was successfully create'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to :products
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path, notice: 'Product was successfully deleted.'
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :desc, :price, :qty, :image)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :desc, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0}
+  validates :qty, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  has_one_attached :image
 end
