8901f3957a4993f72117d446669cefe011c78a8e
maeci <cimafrancamae@gmail.com>
2024-02-19 13:36:52 +0800

    Fix header and footer style

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   validates :content, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0}
   validates :qty, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :image, presence: true
 
   has_one_attached :image
   has_rich_text :content
